e8b4791e6d039863617a7c8cf6fbc52f0f398cb7
kreid86 <93731916+kreid86@users.noreply.github.com>
2021-11-07 13:37:16 -0500

    Update editor.h

--- editor.h
+++ editor.h
@@ -14,7 +14,16 @@ class editor {
 private:
 	LinkedList<string> lines;
 public:
+	/** Default Constructor.
+	@post  Instantiates empty Editor object.
+	*/
 	editor();
+	
+	/** Constructor which instantiates an Editor object by reading a text file into it.
+	@param file  The name of the file to be read in.
+	@pre  The string file denotes a valid text file in the same directory as the project.
+	@post  Prints error message and terminates program if file cannot be opened.
+		Otherwise, each line in file becomes an item in the list lines. */
 	editor(string file);
 	void DisplayLines();
 //	void run();
