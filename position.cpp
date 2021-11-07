8176862986ce442b1917cd50b3301408018459d8
Jerrry-10 <78663777+Jerrry-10@users.noreply.github.com>
2021-11-07 14:36:05 -0500

    Update position.cpp

--- position.cpp
+++ position.cpp
@@ -1,7 +1,18 @@
 #include "position.h"
+#include <xmemory>
 Position::Position() {
 	x = 0;
 	y = 0;
+}
+
+void Position::setX(int newX)
+{
+	x = newX;
+}
+
+void Position::setY(int newY)
+{
+	 y = newY;
 }
 
 int Position::getX() const {
@@ -12,12 +23,3 @@ int Position::getY() const {
 	return y;
 }
 
-void setX(int newX)
-{
-	x = newX;
-}
-
-void setY(int newY)
-{
-	y = newY;
-}
