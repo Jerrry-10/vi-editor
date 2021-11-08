544fe4dd72ffb6196bdde2034370f30b78396444
kreid86 <93731916+kreid86@users.noreply.github.com>
2021-11-08 16:50:59 -0500

    Update position.h
    Finished Javadoc-style comments for methods

--- position.h
+++ position.h
@@ -16,12 +16,22 @@ public:
 	@post  x == incomingX, y == incomingY */
 	Position(int incomingX, int incomingY);
 
+	/**Mutator for x
+	@param newX  The new value to which x is to be set.
+	@post  x == newX */
 	void setX(int newX);
 
+	/**Mutator for y
+	@param newY  The new value to which y is to be set.
+	@post  y == newY */
 	void setY(int newY);
 
+	/**Accessor for x
+	@return  The value of x */
 	int getX() const;
 
+	/**Accessor for y
+	@return  The value of y */
 	int getY() const;
 
 };
