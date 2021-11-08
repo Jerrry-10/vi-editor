7f7574db1452b6e4b7339d9b85408da1e63856d0
kreid86 <93731916+kreid86@users.noreply.github.com>
2021-11-08 00:00:58 -0500

    Update position.h

--- position.h
+++ position.h
@@ -6,11 +6,22 @@ private:
 	int x;
 	int y;
 public:
+	/** Default constructor
+	@post x == 0, y == 0 */
 	Position();
+	
+	/**Constructor
+	@param incomingX  The x-coordinate value.
+	@param incomingY  The y-coordinate value.
+	@post  x == incomingX, y == incomingY */	
 	Position(int incomingX, int incomingY);
+	
 	void setX(int newX);
+	
 	void setY(int newY);
+	
 	int getX() const;
+	
 	int getY() const;
 
 };
