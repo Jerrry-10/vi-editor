ed1f95bba76ef28844a8081a4621747f0bc9e8d3
kreid86 <93731916+kreid86@users.noreply.github.com>
2021-11-08 21:35:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+/** @file main.cpp Page 1
+Program to run a primitive text editor.
+Input:
+	File input: Text (strings) from a .txt file
+	getch/getche: User commands in the form of single characters.
+Output:
+	File output: The updated text, if the user opts to save the changes. Output to same file as input file.
+	cerr: Error message if program terminates because input file will not open.
+	cout: The text and any changes to it, as well as echoing user's end-of-file commands.
+@author: Kevin Reid, Jerry Aviles, & Xhudita Istrefi
+November 8, 2021
+*/
 #include<iostream>
 #include "editor.h"
 
@@ -10,7 +22,7 @@ int main() {
 	//	cin >> name;
 	editor vi{ "test.txt" };
 	vi.displayLines();
-	// run();
+	run();
 
 
 	return 0;
