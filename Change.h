6ffe1674c81759d3fb5b2e106d84ff7d02e494b1
kreid86 <93731916+kreid86@users.noreply.github.com>
2021-11-22 15:25:05 -0500

    Update Change.h

--- Change.h
+++ Change.h
@@ -10,7 +10,7 @@ public:
     
     void setPositionOfDeletedContents(const Position& positionOfDeletedContents);
     
-    string getChangedCharacters() cosnt;
+    string getChangedCharacters() const;
     
     void setChangedCharacters(const string& changedCharacters);
 
