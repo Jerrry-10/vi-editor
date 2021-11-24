a6eb1e2c1990e38aade4abfcc62ee00695bf0f10
kreid86 <93731916+kreid86@users.noreply.github.com>
2021-11-24 12:11:48 -0500

    Update Change.h

--- Change.h
+++ Change.h
@@ -11,8 +11,11 @@ public:
     Position getPositionOfDeletedContents() const;
     
     string getChangedCharacters() const;
+    
+    char getCommand() const;
 
 private:
     string mChangedCharacters; //Can be a line or a single char?
     Position mPositionOfDeletedContents;
+    char mCommand; //'x' or 'd'
 }
