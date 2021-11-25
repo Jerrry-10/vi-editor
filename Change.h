ba723fd82de4c1dc3edbd6cf897791eb2cd8ca37
kreid86 <93731916+kreid86@users.noreply.github.com>
2021-11-25 00:00:59 -0500

    Create Change.h

---
+++ Change.h
@@ -0,0 +1,26 @@
+#include "Position.h"
+using std::string
+
+	class Change
+	{
+	public:
+	    
+	    Change();
+	    
+	    Change (const Position& positionOfDeletedContents, const string& changedCharacters, char command);
+	        
+	    Position getPositionOfDeletedContents() const;
+	    
+	    string getChangedCharacters() const;
+	    
+	    char getCommand() const;
+
+      int getLineNumber() const;
+	
+
+	private:
+	    string mChangedCharacters; //Can be a line or a single char?
+	    Position mPositionOfDeletedContents;
+      int mLineNumber;
+	    char mCommand; //'x' or 'd'
+	}
