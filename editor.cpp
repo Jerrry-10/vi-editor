453d8d9eb93c70c53544049314c1407529358260
kreid86 <93731916+kreid86@users.noreply.github.com>
2021-11-26 00:38:03 -0500

    Update editor.cpp

--- editor.cpp
+++ editor.cpp
@@ -324,7 +324,7 @@ void editor::run() {
             //else do nothing.
             break;
         case 'u':
-                //undo
+            undoLastChange();
         case ':':
             //Create position object which denotes the first space on the fifth empty line (x=0, y= last line + 5).
             Position endOfFile(0, (lines.getLength() + 5));
