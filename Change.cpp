e3eba7452e30b636ece7406434868350ebda0e5b
kreid86 <93731916+kreid86@users.noreply.github.com>
2021-11-28 13:28:26 -0500

    Update Change.cpp

--- Change.cpp
+++ Change.cpp
@@ -1,2 +1,28 @@
 #include "Change.h"
 
+Change::Change()
+{}
+
+Change::Change (const Position& positionOfDeletedContents, const string& changedCharacters, char command)
+:mPositionOfDeletedContents(positionOfDeletedContents), mChangedCharacters(changedCharacters), mCommand(command)
+{}
+
+Position Change::getPositionOfDeletedContents() const
+{
+  return mPositionOfDeletedContents;
+}
+
+string Change::getChangedCharacters() const
+{
+  return mChangedCharacters;
+}  
+
+char Change::getCommand() const
+{
+  return mCommand;
+}
+
+int Change::getLineNumber() const
+{
+  return mLineNumber;
+}
