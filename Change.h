e5a7e690b99e36e0b671293c6d631bcb46f5014f
kreid86 <93731916+kreid86@users.noreply.github.com>
2021-12-02 13:08:43 -0500

    Update Change.h

--- Change.h
+++ Change.h
@@ -1,5 +1,6 @@
-/**@file Change.h
-
+/**@file Change.h Page 1
+Class to store the data pertaining to a change in the text file.
+@author Kevin Reid, Jerry Aviles, Xhudita Istrefi
 */
 
 #ifndef CHANGE_H
